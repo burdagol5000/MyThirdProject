2e8c0e0b7076dda0b2e0ac053e7b91bc38d29778
burdagol5000 <vicenteponteras@gmail.com>
2023-01-22 16:26:15 +0000

    created the try and except for error handling

--- run.py
+++ run.py
@@ -5,23 +5,23 @@ print("Please Choose your category:")
 print("0.Animals")
 print("1.Countries")
 print("2.Gods and Goddesses")
+words=[["elephant","rhinoceros","zebra","giraffe"],
+["philippines","australia","russia","ukraine","ireland"],
+["zeus","thor","odin","hermes","athena","ares","tyr"]]
+def category_selection():
+    chosen_category = int(input("Choose your Category: "))
+    category_words=random.chosen_category=(words[chosen_category])
+    game=random.choice(category_words)
+    print(game)
 try:
-   chosen_category=int(input("Choose your Category: "))
+    category_selection()
 except ValueError:
     print("Please Select from the correct category")
+    category_selection()
 except IndexError:
     print("Please Select from the correct category")
+    category_selection()
 except NameError:
     print("Please Select from the correct category")
+    category_selection()
 
-if chosen_category > len(2):
-            clear()
-            print("No such topic!!! Try again.")
-        elif chosen_category == len(2):
-            print()
-            print("Thank you for playing!")
-            break
-words=[["elephant","rhinoceros","zebra","giraffe"],["philippines","australia","russia","ukraine","ireland"],["zeus","thor","odin","hermes","athena","ares","tyr"]]
-category_words=random.chosen_category=(words[chosen_category])
-game=random.choice(category_words)
-print(game)
