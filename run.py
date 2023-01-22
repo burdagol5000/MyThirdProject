f8b75ff549dbc4c77922708e65c20766a7fcfb3c
burdagol5000 <vicenteponteras@gmail.com>
2023-01-22 16:07:10 +0000

    debugging if user did not select correct category

--- run.py
+++ run.py
@@ -7,11 +7,21 @@ print("1.Countries")
 print("2.Gods and Goddesses")
 try:
    chosen_category=int(input("Choose your Category: "))
-   
 except ValueError:
-    clear()
+    print("Please Select from the correct category")
+except IndexError:
+    print("Please Select from the correct category")
+except NameError:
     print("Please Select from the correct category")
 
+if chosen_category > len(2):
+            clear()
+            print("No such topic!!! Try again.")
+        elif chosen_category == len(2):
+            print()
+            print("Thank you for playing!")
+            break
 words=[["elephant","rhinoceros","zebra","giraffe"],["philippines","australia","russia","ukraine","ireland"],["zeus","thor","odin","hermes","athena","ares","tyr"]]
-random_word=random.chosen_category=(words[chosen_category])
-print(random_word)
+category_words=random.chosen_category=(words[chosen_category])
+game=random.choice(category_words)
+print(game)
