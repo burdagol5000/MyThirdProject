b0dc1f83cc94e51a96b8c7f703eea5f55a42472a
burdagol5000 <vicenteponteras@gmail.com>
2023-01-22 15:12:19 +0000

    created the startup menu and category

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+name=input("Please Enter Your Name:")
+print(f'Welcome to Guess the word!, {name}')
+print("Please Choose your category:")
+print("0.Animals")
+print("1.Countries")
+print("2.Gods and Goddesses")
+try:
+   chosen_category=int(input("Choose your Category: "))
+   
+except ValueError: 
+    clear()
+    print("Please Select from the correct category")
+    
+words=[["elephant","rhinoceros","zebra","giraffe"],["philippines","australia","russia","ukraine","ireland"],["zeus","thor","odin","hermes","athena","ares","tyr"]]
+random_word=random.chosen_category(words[chosen_category])
+print(random_word)
